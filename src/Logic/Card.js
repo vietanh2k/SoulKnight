0d320d954ea1eb4af666ee86a9a8488485f4eec6
Nguyen Minh Quang <63718153+nbtpj@users.noreply.github.com>
2022-11-04 16:38:27 +0700

    add level property

--- src/Logic/Card.js
+++ src/Logic/Card.js
@@ -3,6 +3,7 @@ var Card = cc.Class.extend({
         this.id= byte_buffer.getInt();
         this.name= byte_buffer.getString();
         this.type= byte_buffer.getByte();
+        this.level= byte_buffer.getInt();
         this.quantity= byte_buffer.getInt();
         this.attackSpeed= byte_buffer.getFloat();
         this.attackRange= byte_buffer.getFloat();
