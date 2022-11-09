a288d4717be5e4f281ed5cd4c3384cdf9ca3969b
nganhtu <nganhtu0712@gmail.com>
2022-11-09 11:59:12 +0700

    Update Chest.js

--- src/Logic/Chest.js
+++ src/Logic/Chest.js
@@ -1,23 +1,23 @@
-var Chest  = cc.Class.extend({
+var Chest = cc.Class.extend({
     ctor: function (byte_buffer) {
-        this.id= byte_buffer.getInt();
-        this.type= byte_buffer.getByte();
-        this.remainingTimeInSecond= this.setRemainingTime(byte_buffer.getLong());
-        this.openTimeInSecond= byte_buffer.getLong(); //thời gian mở rương cố định: ví d: 3600s, 7200s,...
+        this.id = byte_buffer.getInt();
+        this.type = byte_buffer.getByte();
+        this.setRemainingTime(byte_buffer.getLong());
+        // this.openTimeInSecond= byte_buffer.getLong(); //thời gian mở rương cố định: ví d: 3600s, 7200s,...
 
     },
     /**
      * Cài thời gian đếm ngược mở rương
      * Nếu rương chưa start cool down, remaining time phải có giá trị 1000000*/
-    setRemainingTime:function (time_in_second){
+    setRemainingTime: function (time_in_second) {
         // update UI gì đó.... (nếu có)
-        this.remainingTimeInSecond = time_in_second;
+        this.openOnServerTimeStamp = time_in_second;
         this._init_time_stamp = Date.now() / 1000
     },
     // todo: thêm các hàm
-    onOpenNow: function (){
+    onOpenNow: function () {
         // testnetwork.connector.sendOpenChestRequest(this);
-        if (Date.now() / 1000 - this._init_time_stamp + 1 > this.remainingTimeInSecond){
+        if (Date.now() / 1000 - this._init_time_stamp + 1 > this.remainingTimeInSecond) {
             testnetwork.sendOpenChestRequest(this);
         } else {
             cc.log("not time!")
@@ -26,20 +26,24 @@ var Chest  = cc.Class.extend({
     /**
      * Gửi yêu cầu bắt đầu cooldown
      * */
-    startCoolDown: function (){
+    startCoolDown: function () {
         testnetwork.connector.sendStartCoolDownRequest(this);
     },
     /**
      * Xử lý phải hồi yêu cầu bắt đầu cooldown
      * @return{String} status: Phản hồi dạng string từ server
      * */
-    onStartCoolDown: function (byte_buffer){
+    onStartCoolDown: function (byte_buffer) {
         // đặt lại mốc remaining time cập nhập từ server
         this.setRemainingTime(byte_buffer.getLong());
         // lấy phản hồi
-        let res_status = byte_buffer.getString();
-        return res_status
-
-
+        res_status = byte_buffer.getString();
+        return res_status;
+    },
+    getGemToOpen: function () {
+        return 20;
+    },
+    getGoldToOpen: function () {
+        return 20;
     }
 })
