6d7f87b49bf477958050685743e15ac03aa3ba8e
nganhtu <60926756+nganhtu@users.noreply.github.com>
2022-11-30 10:43:18 +0700

    misc

--- src/UI/GameUI.js
+++ src/UI/GameUI.js
@@ -100,7 +100,7 @@ var GameUI = cc.Layer.extend({
                         if(this._gameStateManager.playerA.energy >= this.listCard[this.cardTouchSlot - 1].energy){
                             this.createObjectByTouch = true
                         } else {
-                            Utils.addToastToRunningScene('Không đủ mana!');
+                            Utils.addToastToRunningScene('Không đủ năng lượng!');
                             this.resetCardTouchState()
                         }
                     }
@@ -581,7 +581,7 @@ var GameUI = cc.Layer.extend({
                             return;
                         }
                     }else{
-                        Utils.addToastToRunningScene('Không đủ mana!');
+                        Utils.addToastToRunningScene('Không đủ năng lượng!');
                         this.resetCardTouchState()
                     }
 
