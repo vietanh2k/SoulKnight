76839c08cec92f75d7553ee8bc71957528e73b38
Nguyen Huu Kiet <kietnh2@vng.com.vn>
2022-11-30 17:39:20 +0700

    - update ninja

--- src/Logic/utils/Random.js
+++ src/Logic/utils/Random.js
@@ -8,13 +8,21 @@ const Random = {
         this.m_z = (987654321 - i) & this.mask;
     },
 
+    random: function () {
+        this.m_z = (36969 * (this.m_z & 65535) + (this.m_z >> 16)) & this.mask;
+        this.m_w = (18000 * (this.m_w & 65535) + (this.m_w >> 16)) & this.mask;
+        var result = ((this.m_z << 16) + (this.m_w & 65535)) >>> 0;
+        result /= 4294967296;
+        return result;
+    },
+
     range: function (min, max) {
-        return Math.random() * (max - min) + min
+        return this.random() * (max - min) + min
     },
 
     rangeInt: function (min, max) {
         min = Math.ceil(min)
         max = Math.floor(max)
-        return Math.floor(Math.random() * (max - min + 1) + min)
+        return Math.floor(this.random() * (max - min + 1) + min)
     }
 }
