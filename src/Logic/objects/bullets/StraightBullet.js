e517fb8135dee537d54fb3ee7e3d398cf26a3a07
nganhtu <60926756+nganhtu@users.noreply.github.com>
2022-12-20 11:16:23 +0700

    thay getObjectInRange bằng queryEnemiesCircle

--- src/Logic/objects/bullets/StraightBullet.js
+++ src/Logic/objects/bullets/StraightBullet.js
@@ -17,7 +17,7 @@ let TWizardBullet = Bullet.extend({
 
     explose: function (playerState, pos) {
         const map = playerState.getMap();
-        let objectList = map.getObjectInRange(pos, this.radius);
+        let objectList = map.queryEnemiesCircle(pos, this.radius * MAP_CONFIG.CELL_WIDTH);
         let damage = this.damage;
         if (this.level === 3 && objectList.length > 5) {
             damage += 10;
@@ -67,7 +67,7 @@ let TOilGunBullet = Bullet.extend({
 
     explose: function (playerState, pos) {
         const map = playerState.getMap();
-        let objectList = map.getObjectInRange(pos, this.radius);
+        let objectList = map.queryEnemiesCircle(pos, this.radius * MAP_CONFIG.CELL_WIDTH);
         for (let object of objectList) {
             if (this.canAttack(object) && (this.targetType === 'all' || this.targetType === object.class)) {
                 object.takeDamage(playerState, this.damage, this.fromTower);
