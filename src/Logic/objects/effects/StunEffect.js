9611b5ad85e2c2307666006e25e57beed6122c7c
nganhtu <60926756+nganhtu@users.noreply.github.com>
2022-12-27 09:12:28 +0700

    thêm stun animation

--- src/Logic/objects/effects/StunEffect.js
+++ src/Logic/objects/effects/StunEffect.js
@@ -4,20 +4,42 @@ const StunEffect = Effect.extend({
         this._super(time);
 
         this.target = target;
-        this.target.inactiveSourceCounter++;
-        // todo animation stun
-        if(this.target.concept === 'monster' && !this.target.isDestroy) {
-            this.target.play(-1);
+
+        if (this.target.stunUI === undefined) {
+            this.target.stunUI = new sp.SkeletonAnimation(asset.effectStun_json, asset.effectStun_atlas);
+            this.target.stunUI.attr({
+                x: this.target.width * 0.5,
+                y: this.target.height * 0.8,
+                scale: 0.8,
+            });
+            this.target.addChild(this.target.stunUI);
+            this.target.stunUI.setAnimation(0, 'animation', true);
         }
+
+        if (this.target instanceof Monster && !this.target.isDestroy) {
+            this.target.play(-1);
+            this.target.inactiveSourceCounter++;
+            this.target.active = false;
+        } else {
+            this.target.active = false;
+        }
+
         this.target.retain();
     },
 
     destroy: function (playerState) {
-        this.target.inactiveSourceCounter--;
-        if (this.target.concept === 'monster' && !this.target.isDestroy) {
+        if (this.target instanceof Monster && !this.target.isDestroy) {
             this.target.play(0);
+            if ((--this.target.inactiveSourceCounter) === 0) {
+                this.target.active = true;
+            }
+        } else {
+            this.target.active = true;
         }
-        // todo animation stun
+        if (this.target.stunUI !== undefined) {
+            this.target.stunUI.removeFromParent(true);
+            this.target.stunUI = undefined;
+        }
         this.target.stunEffect = undefined;
         this.target.release();
     }
