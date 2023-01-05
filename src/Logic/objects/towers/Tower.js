c74b9e41ad0dd74d257cd63c970ad54789c58cb4
nganhtu <60926756+nganhtu@users.noreply.github.com>
2023-01-05 17:34:39 +0700

    fix đồng bộ

--- src/Logic/objects/towers/Tower.js
+++ src/Logic/objects/towers/Tower.js
@@ -268,27 +268,29 @@ var Tower = TowerUI.extend({
         }
         this.status = 'readyToFire';
 
-        if (this.getPending() > 0) {
-            this.updatePending(dt);
-        } else {
-            this.visible = true;
-            if (this.attackCooldown <= 0) {
-                this.findTargets(playerState)
-                if (this.target.length > 0) {
-                    for (let i = 0; i < this.target.length; i++) {
-                        if ("monster" === this.target[i].concept) {
-                            this.fire();
-                            this.status = 'cooldowning';
-                            this.attackCooldown = self.getAttackSpeed();
-                            break;
+        if (this.active) {
+            if (this.getPending() > 0) {
+                this.updatePending(dt);
+            } else {
+                this.visible = true;
+                if (this.attackCooldown <= 0) {
+                    this.findTargets(playerState);
+
+                    if (this.target.length > 0) {
+                        for (let i = 0; i < this.target.length; i++) {
+                            if ("monster" === this.target[i].concept) {
+                                this.fire();
+                                this.status = 'cooldowning';
+                                this.attackCooldown = self.getAttackSpeed();
+                                break;
+                            }
                         }
                     }
+                } else {
+                    this.attackCooldown -= dt;
                 }
-            } else {
-                this.attackCooldown -= dt;
             }
         }
-
     },
 
     checkIsTarget: function (another) {
